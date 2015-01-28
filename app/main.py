b25c8b3ebc24a77476296c493fab4a2085bc3fdc
Jesse B Miller <jmiller@adops.com>
2015-01-28 15:42:42 -0600

    updates version number

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import sys
 import argparse
 import commands
 
-__version__ = (0, 2, 1, 'alpha', 0)
+__version__ = (0, 2, 2, 'alpha', 0)
 
 def fmt_version(type='long', v=__version__):
     """ format the version in long or short form """
