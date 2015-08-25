3343c0b7899598216f86dcdd34e8749960f9583d
Jesse B Miller <jmiller@adops.com>
2015-08-25 11:27:45 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=['app'],
     install_requires=[
         'fabric',
-        'http://github.com/OAODEV/python-gnupg/tarball/master#egg=gnupg-0.3.2',
+        'http://github.com/OAODEV/python-gnupg#egg=gnupg',
         'psycopg2',
         'sqlalchemy',
         ],
